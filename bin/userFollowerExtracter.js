c52ee72f8cefc82197f91d4f0561db536a9c5c39
Gregory Thompson <greg@mashwork.com>
2013-08-27 13:18:31 -0400

    function which takes set of pages and finds common followers

--- bin/userFollowerExtracter.js
+++ bin/userFollowerExtracter.js
@@ -1,14 +1,49 @@
-var twitter = require("ntwitter");
+var twitter = require("ntwitter"),
+	_ = require("lodash"),
+	async = require("async"),
+	twit = new twitter(require("../config/config.js").config),
+	screenNames = _.rest(process.argv,2);
 
-var twit = new twitter(require("../config/config.js").config);
+console.log("screenNames = %j", screenNames);
+console.log("https://api.twitter.com/1.1/followers/ids.json")
 
-twit
-	.verifyCredentials(function (err, data) {
-		console.log(data);
-	})
-	.updateStatus(
-		'Test tweet from ntwitter/' + twitter.VERSION,
-		function (err, data) {
-			console.log(data);
-		}
-	);
+var timeout = 500;
+
+async.mapSeries(
+	screenNames,
+	function(screenName, iterCallback){
+		getFollowers(screenName, iterCallback);
+	},
+	function(err, screenNamesArray){
+		var intersectIds = _.intersection.apply(this, screenNamesArray);
+		console.log("intersectIds = %j", intersectIds);
+	}
+);
+
+function getFollowers(screenName, callback){
+	var collectedFollowers = [];
+
+	function getFollowersPartial(cursor){
+		twit
+			.get(
+				"https://api.twitter.com/1.1/followers/ids.json",
+				{ screen_name: screenName,
+					cursor: cursor },
+				function (err, data) {
+					if (err){ return callback(err, null);}
+
+					collectedFollowers = collectedFollowers.concat(data.ids);
+					console.log("screenName " + screenName + " number recieved so far " + collectedFollowers.length);
+					
+					setTimeout(function () {
+						if(data.next_cursor != 0){return getFollowersPartial(data.next_cursor); }
+
+						callback(null, collectedFollowers);
+						},
+						timeout
+					);
+				}
+			);
+	}
+	getFollowersPartial(-1);
+}
